67c57bef2d8c53092029dc3eda54e6e3d943d68d
Sho <harimoto@ulaberta.ca>
2024-11-12 13:57:01 -0700

    some of the login screen

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+
+
+# This is the function tp sign up
+def sign_up():
+    name = input("Enter your name: ")
+    email = input("Enter your email: ")
+    phone = input("Enter your phone: ")
+    pwd = input("Enter your password: ")
+    if (1):
+        print("Congratulation! You successfully sign in! \n")
+        print("Here is your user id: " + "1")
+    else:
+        print("Your use id is already in the datebase")
+
+
+# This is the function to login
+def login():
+    usr = input("Enter User ID: ")
+    pwd = input("Enter Password: ")
