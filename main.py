b873338f3591ed89d4ede200da43aa5e57a6e355
Hana Awaleh <hanaawaleh@Hana-MacBook-Air-2.local>
2024-11-12 15:15:01 -0700

    adding new main changes

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
+# main.py
+
 import sqlite3
+from auth import register_user, login_user  # import functions from auth.py
+
+# function to connect to db
+def connect_db():
+    connection = sqlite3.connect("prj-sample.db")
+    cursor = connection.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")  # enable foreign keys
+    return connection, cursor
 
 def login_screen():
-    print("=== Welcome to Twitter ===")
-
+    print("\n=== Welcome to Twitter ===")
     print("1. Login")
     print("2. Register")
     print("3. Exit")
     option = input("\nChoose an option: ")
     return option
 
+# main
 if __name__ == "__main__":
+    connection, cursor = connect_db() # db connection
+
     while True:
         option = login_screen()
-        
         if option == '1':
-            # login function
-            pass
+            user_id = login_user(cursor)  
+            if user_id:
+                print(f"User {user_id} is now logged in.")
         elif option == '2':
-            # sign up function
-            pass
+            register_user(cursor, connection)  
         elif option == '3':
             print("\nExiting program...")
             break
         else:
             print("\nInvalid option. Please try again.")
 
+    connection.close()
